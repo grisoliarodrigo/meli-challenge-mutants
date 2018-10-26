eb4f8bf3669b7caab0af25c8136b64cd60837668
Rodrigo Grisolía <grisoliarodrigo@gmail.com>
2018-10-26 16:30:17 -0300

    Tests for personService added.

--- src/test/java/meli/mutants/domain/PersonServiceTest.java
+++ src/test/java/meli/mutants/domain/PersonServiceTest.java
@@ -9,33 +9,43 @@ import org.mockito.MockitoAnnotations;
 import junit.framework.Assert;
 import meli.mutants.repository.PersonRepository;
 import meli.mutants.service.PersonService;
+import meli.mutants.util.detector.exceptions.InvalidDNAException;
 
-public class PersonTest {
+public class PersonServiceTest {
 	
 	@Mock PersonRepository personRepository;
 	@InjectMocks PersonService personService = new PersonService();;
 	
+	String[] mutantDna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
+	String[] notMutantDna = {"GGGGCC","ATATAT","GCGCGC","ATATAT","GCGCGC","ATATAT"};
+	String[] invalidDna = {"Y"};
+
+	Person person;
+ 	
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
+		person = new Person();
 	}
 	
 	@Test
-	public void testIsMutant() {
-		Person person = new Person();
-		String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
-		person.setDna(dna);
+	public void test_is_mutant() {
+		person.setDna(mutantDna);
 		personService.save(person);
 		Assert.assertTrue(person.isMutant());
 	}
 	
 	@Test
-	public void testIsNotMutant() {
-		Person person = new Person();
-		String[] dna = {"GGGGCC","ATATAT","GCGCGC","ATATAT","GCGCGC","ATATAT"};
-		person.setDna(dna);
+	public void test_is_not_mutant() {
+		person.setDna(notMutantDna);
 		personService.save(person);
 		Assert.assertFalse(person.isMutant());
+	}
+	
+	@Test(expected = InvalidDNAException.class)
+	public void test_save_fails_with_invalid_dna() {
+		person.setDna(invalidDna);
+		personService.save(person);
 	}
 	
 }
--- src/test/java/meli/mutants/util/detector/MutantDetectorTest.java
+++ src/test/java/meli/mutants/util/detector/MutantDetectorTest.java
@@ -7,86 +7,86 @@ import meli.mutants.util.detector.exceptions.InvalidDNAException;
 public class MutantDetectorTest {
 	
 	@Test
-	public void testIsMutant() {
-		String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
+	public void test_is_mutant() {
+		String[] dna = {"AAAAAA","CCCCCC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
 		Assert.assertTrue(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void testIsNotMutant() {
+	public void test_is_not_mutant() {
 		String[] dna = {"GGGGCC","ATATAT","GCGCGC","ATATAT","GCGCGC","ATATAT"};
 		Assert.assertFalse(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void testIsMutantWithTwoSequences() {
+	public void test_is_mutant_two_sequences() {
 		String[] dna = {"AAAAGA","CCGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
 		Assert.assertTrue(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void testIsMutantWithTwoDiagonals() {
+	public void test_is_mutant_with_two_diagonals() {
 		String[] dna = {"ATGTGA","CATTGC","TTATTT","TGAAGG","GCGTCA","TCACTG"};
 		Assert.assertTrue(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void testIsNotMutantWithOneDiagonal() {
+	public void test_is_not_mutant_with_one_diagonal() {
 		String[] dna = {"ATGGGA","CATTGC","TTATTT","TGAAGG","GCGTCA","TCACTG"};
 		Assert.assertFalse(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void testIsMutantWithTwoDiagonalsInside() {
+	public void test_is_mutant_with_two_diagonals_inside() {
 		String[] dna = {"GTGTGA","CATTGC","TTATTT","TGAAGG","GCGTAA","TCACTG"};
 		Assert.assertTrue(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void testIsNotMutantOneHorizontalFiveLettersSequence() {
+	public void test_is_not_mutant_with_one_horizontal_five_letter_sequence() {
 		String[] dna = {"ATATAT", "GGGGGC", "ATATAT", "GCGCGC", "ATATAT", "GCGCGC"};
 		Assert.assertFalse(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void oneVerticalFiveLettersSequenceIsNotMutant() {
+	public void test_is_not_mutant_with_one_vertical_five_letter_sequence() {
 		String[] dna = {"ATATAT", "ACGCGC", "ATATAT", "ACGCGC", "ATATAT", "GCGCGC"};
 		Assert.assertFalse(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void oneDiagonalFowardFiveLettersSequenceIsNotMutant() {
+	public void test_is_not_mutant_with_one_diagonal_foward_five_letter_sequence() {
 		String[] dna = {"ATATAT", "GAGCGC", "ATATAT", "GCGAGC", "ATATAT", "GCGCGC"};
 		Assert.assertFalse(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void oneDiagonaBackwardlFiveLettersSequenceIsNotMutant() {
+	public void test_is_not_mutant_with_one_diagonal_backward_five_letter_sequence() {
 		String[] dna = {"ATATAT", "GCGCTC", "ATATAT", "GCTCGC", "ATATAT", "GCGCGC"};
 		Assert.assertFalse(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test
-	public void testIsMutantWithOneSequenceOfFiveAndOneSequenceOfFour() {
+	public void test_is_mutant_with_one_sequence_of_five_and_one_sequence_of_four() {
 		String[] dna = {"AAAAAT","GCGCGC","GTATAT","GCGCGC","GTATAT","CCGCGC"};
 		Assert.assertTrue(new MutantDetector(dna).isMutant());
 	}
 	
 	@Test(expected = InvalidDNAException.class)
-	public void testInvalidTableSize() {
+	public void test_invalid_table_size() {
 		String[] dna = {"AAA","GCG","GTA"};
 		new MutantDetector(dna).isMutant();
 	}
 	
 	@Test(expected = InvalidDNAException.class)
-	public void testInvalidRowLength() {
+	public void test_invalid_row_length() {
 		String rowOfFour = "AAAA";
 		String[] dna = {"AAAAAT",rowOfFour,"GTATAT","GCGCGC","GTATAT","CCGCGC"};
 		new MutantDetector(dna).isMutant();
 	}
 	
 	@Test(expected = InvalidDNAException.class)
-	public void testInvalidCharacter() {
+	public void test_invalid_character() {
 		char x = 'X';
 		String[] dna = {"AAAAAT","GCGCGG","GTATAT","GCGCGC","GTATAT",("CCGCG" + x)};
 		new MutantDetector(dna).isMutant();
